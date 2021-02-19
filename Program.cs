7bb85d6e493bb15a3d46fef5ec0f67bc2e8573e6
Charlie <charlesrichardson123000@gmail.com>
2021-02-19 13:45:15 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URL_Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
